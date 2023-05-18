0d1c904cbe740577b3e642150845869cbf4bdee8
Replit user <>
2023-05-18 21:29:26 +0000

    data types

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-nome = input('What is your name? ') or '' #or can be used as null coalescing operator
-print('hellooo, ' + nome)  #accepts single or double quotes
+#The 8 fundamental data types of Python 3.
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+#Classes -> custom types
+  #To be seen in the future
+
+#Specialized Data Types
+  #Can be taken from Modules (e.g. kind of extensions)
+
+None #this is a little "special"
+  #means nothing
