101cfb18b97aa7cd86662b1b751f4978e21e52e8
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-06-07 14:42:00 -0300

    session3/lesson24 WIP

--- section3_basics/23_devFundamentals1.py
+++ section3_basics/23_devFundamentals1.py
@@ -2,7 +2,7 @@
 
 print(
     20 - 3 * 4,
-    "because 20 - 3 * 4 has the precedence of multiplication over subtraction",
+    "because in '20 - 3 * 4' the precedence of multiplication is over subtraction",
 )
 print((20 - 3) + 2**2)
 print((5 + 4) * 10 / 2)  # divisions always output a float
@@ -13,9 +13,10 @@ print((5 + 4) * 10 / 2)  # divisions always output a float
 # **
 
 print((5 + 4) * 10 / 2)  # divisions always output a float
-print(
-    5 + 4 * 10 / 2
-)  # multiplication and division have the same precedence and are evaluated from left to right
+print(5 + 4 * 10 / 2)
+# multiplication and division have the same precedence and are
+# evaluated from left to right
+print(5 * 2 / 3)
 
 # Precedence:
 # ()
