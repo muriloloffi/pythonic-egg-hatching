41a918efae5683fec403640c8eefddbbae0d68c6
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-06-08 08:54:37 -0300

    sessions3/lesson28

---
+++ section3_basics/28_expressionsVsStatements.py
@@ -0,0 +1,7 @@
+# Expressions vs Statements
+
+iq = 100
+user_age = iq / 5  # expression: a piece of code that produces a value
+
+# a statement is an entire line of code that performs an action:
+# in the examples above, binding values to variables are statements
