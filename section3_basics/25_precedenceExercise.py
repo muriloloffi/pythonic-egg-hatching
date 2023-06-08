65a7386af02c4d6249359734711f9bd460b713fe
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-06-08 10:16:00 -0300

    formatting

--- section3_basics/25_precedenceExercise.py
+++ section3_basics/25_precedenceExercise.py
@@ -2,12 +2,17 @@
 # Try to write down your answer before and see how you do...
 # keep it mind I made it a little tricky for you :)
 
-print((5 + 4) * 10 / 2, "float")  # = 9 * 10 / 2 = 90/2 = 45.0 = float;
+print((5 + 4) * 10 / 2, "float")
+# = 9 * 10 / 2 = 90/2 = 45.0 = float;
 
-print(((5 + 4) * 10) / 2, "float")  # = (9 * 10) / 2 = 90 / 2 = 45.0 = float;
+print(((5 + 4) * 10) / 2, "float")
+# = (9 * 10) / 2 = 90 / 2 = 45.0 = float;
 
-print((5 + 4) * (10 / 2), "float")  # = 9 * 5.0 = 45.0 = float;
+print((5 + 4) * (10 / 2), "float")
+# = 9 * 5.0 = 45.0 = float;
 
-print(5 + (4 * 10) / 2, "float")  # = 5 + 40 / 2 = 5 + 20.0 = 25.0 = float;
+print(5 + (4 * 10) / 2, "float")
+# = 5 + 40 / 2 = 5 + 20.0 = 25.0 = float;
 
-print(5 + 4 * 10 // 2, "integer")  # = 5 + 40 // 2 = 5 + 20 = 25 = integer;
+print(5 + 4 * 10 // 2, "integer")
+# = 5 + 40 // 2 = 5 + 20 = 25 = integer;
