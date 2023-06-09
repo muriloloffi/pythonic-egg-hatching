88f9ccfe4f364e5828eab8008e6b8a9bd22236ba
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-06-09 11:52:43 -0300

    session3/lesson30

---
+++ section3_basics/30_strings.py
@@ -0,0 +1,25 @@
+# Strings
+
+# Strings are a piece of text
+# Can be enclosed in single or double quotes
+
+str1 = "Hello World"
+
+print(str1, type(str1))
+
+username = "superuser"
+password = "supersecret"
+
+long_string = """
+wow
+0 0
+___
+"""
+
+print(long_string)
+
+first_name = "Andrei"
+last_name = "Silva"
+full_name = first_name + " " + last_name
+
+print(full_name)
