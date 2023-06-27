f6b979888154053be04addeff4171117c2d5a09b
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-06-26 21:24:17 -0300

    session3/lesson38 - booleans

---
+++ section3_basics/38_booleans.py
@@ -0,0 +1,18 @@
+# Booleans
+
+# classic boolean behavior
+
+name = "Andrei"  # string
+is_cool = False  # boolean
+
+is_cool = True
+
+# Booleans can be used to control the flow of a program.
+# It's a cornerstone of programming.
+
+# A bool conversion function converts values to booleans according to the following rules:
+# 1. False, None, 0, "", (), [], {} are all False
+# 2. Everything else is True
+
+print(bool(0))  # False
+print(bool(1))  # True
