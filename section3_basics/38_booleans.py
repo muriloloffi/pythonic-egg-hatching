6517d031087a0b1310f658bb628d4c094d5a982d
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-06-26 21:27:09 -0300

    additional information on Booleans

--- section3_basics/38_booleans.py
+++ section3_basics/38_booleans.py
@@ -10,9 +10,15 @@ is_cool = True
 # Booleans can be used to control the flow of a program.
 # It's a cornerstone of programming.
 
+if is_cool:  # if is_cool is True
+    print("Andrei is cool")
+else:
+    print("Andrei is not cool")
+
 # A bool conversion function converts values to booleans according to the following rules:
 # 1. False, None, 0, "", (), [], {} are all False
 # 2. Everything else is True
 
 print(bool(0))  # False
 print(bool(1))  # True
+print(bool("False"))  # True, because it's a non-empty string
