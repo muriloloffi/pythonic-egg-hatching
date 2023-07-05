1656b7939ad884f195021a5d6c1c7508c85820f2
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-07-04 22:12:31 -0300

    WIP session3/lesson42 - lists

--- section3_basics/42_list.py
+++ section3_basics/42_list.py
@@ -25,3 +25,14 @@ li4 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 # This is the first data structure we will look at. We will see that lists are very
 # versatile and can be used in many different ways.
+
+# Taking another example, if we had a shopping cart, we could store the items in a list:
+
+amazon_cart = ["notebooks", "sunglasses", "toys", "grapes"]
+
+print(len(amazon_cart))  # This will print the first item in the list
+
+print(
+    """personal note here: I am not sure whether len() is the most genius or non-sense
+    way a programming language implements item counting from different data types."""
+)
