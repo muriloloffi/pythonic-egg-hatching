02d84eb99f6dd121a41660edc9fd6f155a68d945
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-07-12 15:54:00 -0300

    session3/lesson52 - dev fundamentals 3

---
+++ section3_basics/52_devFundamentals3.py
@@ -0,0 +1,9 @@
+# Developer Fundamentals 3
+
+# Understanding Data Structures: An experienced programmer knows when to use a data
+# structure over another, what the tradeoffs are. This takes practice. When to use each?
+# Lists are good to keep data in order, and to access it by index. Dictionaries are good
+# to access data by key, but they don't keep the order of the data. Also, dictionaries
+# hold more information than lists. That is, a list is a sequence of elements, while a
+# dictionary is a sequence of key-value pairs. Later on, we'll see tuples and sets,
+# which are also data structures. We'll see when to use each one of them.
