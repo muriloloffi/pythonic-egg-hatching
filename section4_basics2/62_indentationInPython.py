2e5a2eb58f305e64788029b6fb3d8059f2eb9d06
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-10-30 21:07:56 -0300

    section4/lesson62 - truthy vs falsey

---
+++ section4_basics2/62_indentationInPython.py
@@ -0,0 +1,10 @@
+# Indentation in Python
+
+# Indentation refers to the spaces at the beginning of a code line.
+# Where in other programming languages the indentation in code is for readability only,
+# the indentation in Python is very important.
+# Python uses indentation to indicate a block of code.
+# It will give you an error if you skip the indentation:
+# Example
+# if 5 > 2:
+# print("Five is greater than two!")
