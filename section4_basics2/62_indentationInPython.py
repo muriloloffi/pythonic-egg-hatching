7150580ea0ee8ff1ddb310a10489e024a43c15ed
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-11-09 22:08:48 -0300

    section4/lesson64 - Ternary Operator

--- section4_basics2/62_indentationInPython.py
+++ section4_basics2/62_indentationInPython.py
@@ -7,4 +7,4 @@
 # It will give you an error if you skip the indentation:
 # Example
 # if 5 > 2:
-# print("Five is greater than two!")
+#   print("Five is greater than two!")
---
+++ section4_basics2/64_ternaryOperators.py
@@ -0,0 +1,8 @@
+# Ternary Operators
+
+# Teranary operators are a way to write if-else statements in one line
+# Syntax: <value if true> if <condition> else <value if false>
+
+# Example:
+buddy = "dog"
+print(f"Hey {buddy}" if buddy == "dog" else "buddy is not a dog")
