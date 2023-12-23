ea4d3028d88d4289273f29813a997a65caac3d9c
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-12-22 23:03:19 -0300

    section4/lesson76 - break, continue, pass

---
+++ section4_basics2/76_breakContinuePass.py
@@ -0,0 +1,10 @@
+# Break, continue and pass keywords
+
+# Break keyword stops the execution of the loop, be it for or while
+
+# Continue tells the interpreter that in a singular iteration of the loop, every
+# statement after the continue should be ignored and it'll continue from the
+# very next element of the data structure that is being iterated over.
+
+# Finally, pass is just a placeholder, a way to make a loop exist without
+# requiring us to have any logic placed and without throwing any errors
