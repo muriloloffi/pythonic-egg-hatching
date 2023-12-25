54877feb62806d685c94f614add52b7d3c075278
Murilo <19322082+muriloloffi@users.noreply.github.com>
2023-12-25 11:28:06 -0300

    section4/lesson 79 - find duplicates exercise

---
+++ section4_basics2/79_findDuplicatesExercise.py
@@ -0,0 +1,11 @@
+# Exercise: check for duplicates in list:
+some_list = ['a', 'b', 'c', 'b', 'd', 'm', 'n', 'n']
+
+# for each item of the list, check how many time it appears, if it's greater than 1, print it
+
+duplicate_list = []
+for item in some_list:
+  if some_list.count(item) > 1:
+    duplicate_list.append(item) if item not in duplicate_list else None
+
+print(duplicate_list)
