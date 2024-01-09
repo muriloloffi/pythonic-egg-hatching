406c90da1a739eb3962e759ce904f380618a28ec
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-01-09 10:44:29 -0300

    WIP section4/lesson85 - clean code

---
+++ section4_basics2/86_cleanCode.py
@@ -0,0 +1,5 @@
+# Clean Code
+
+# Writing clean code translates to writing concise code with well named
+# functions, variables, classes and methods, that reduces the cognitive load
+# on the programmer when reading that code.
