c057b7606de351224d5305cc45a00d1225b8d817
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-01-09 11:50:22 -0300

    section4/lesson88 - exercise: functions

---
+++ section4_basics2/88_exerciseFunctions.py
@@ -0,0 +1,28 @@
+# Functions Exercise
+
+def highest_even(li: list) -> int:
+  highest_even = 0
+  for number in li:
+    if number % 2 == 0:
+      highest_even = number if highest_even < number else highest_even
+  return highest_even
+
+print(highest_even([10,2,3,4,8,11]))
+
+def highest_evenV2(li: list) -> int:
+  highest_number = max(li)
+  while highest_number % 2 != 0:
+    li.remove(highest_number)
+    highest_number = max(li)
+  return highest_number
+
+print(highest_evenV2([10,2,3,4,8,11]))
+
+def highest_evenV3(li: list) -> int:
+  evens = []
+  for item in li:
+    if item % 2 == 0:
+      evens.append(item)
+  return max(evens)
+
+print(highest_evenV3([10,2,3,4,8,11]))
