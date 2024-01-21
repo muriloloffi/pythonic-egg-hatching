04f73722ca746e9e4438937936b282b02fa452f2
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-01-20 22:33:59 -0300

    section4/lesson93 - nonlocal keyword

---
+++ section4_basics2/93_nonlocalKeyword.py
@@ -0,0 +1,17 @@
+# nonlocal Keyword
+
+# This keyword allows us to ask the interpreter to use a variable outside of the
+# scope of a function, but avoid using the global scope.
+
+x = 'local'
+def outer():
+  x = 'local'
+  def inner():
+    nonlocal x # if line 8 is commented, the interpreter returns an error
+    x = 'nonlocal'
+    print('inner:', x)
+  
+  inner()
+  print('outer:', x)
+  
+outer()
