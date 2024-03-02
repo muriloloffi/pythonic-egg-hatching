bec4c759b368a36609fc2482c4f375168f802fdd
Murilo <19322082+muriloloffi@users.noreply.github.com>
2024-03-02 11:37:12 -0300

    section6/lesson129 - inheritance

--- section6_OOP/126_encapsulation.py
+++ section6_OOP/126_encapsulation.py
@@ -10,7 +10,7 @@
 # not use getters and setters. We will see in the next paragraph how Python
 # deals with attribute access. This reminder is just for developers who have
 # already used another programming language, specially Java and PHP. Now, if
-# you're learning Python as your first programming language, just read along
+# you're learning Python as your first programming language, just read along the
 # information in the following lines as a new lesson, just like everything we
 # have seen so far.
 
@@ -23,11 +23,76 @@
 # [1][https://en.wikipedia.org/wiki/Uniform_access_principle]
 # [2][https://stackoverflow.com/questions/4555932/should-i-use-public-attributes-or-public-properties-in-python]
 
+
+class Foo:
+    def __init__(self) -> None:
+        self._bar = 0
+
+    @property
+    def bar(self) -> int:
+        return self._bar
+
+    @bar.setter
+    def bar(self, bar_value: int) -> None:
+        self._bar = bar_value
+
+
+x = Foo()
+x.bar = 5
+print(x.bar)
+
+
 # Abstraction
 
 # The second pillar, abstraction, states that an object should hide its internal
-# implementation. So that developers that use the object and its methods don't
-# need to know the details of implementation of its methods. When we run a list
-# method, for instance, we don't need to know how pop() is implemented, as long
-# as we know what to expect as a return and treat the data type accordingly,
-# every should go well.
+# implementation, so that developers that use the object and its methods don't
+# need to know the details of this implementation. When we run a list method,
+# for instance, we don't need to know how pop() is implemented, as long as we
+# know what to expect as a return and treat the data type accordingly,
+# everything should go well.
+
+
+# Inheritance
+
+# In OOP, inheritance is the practice of creating a relationship between classes
+# with a common behavior through a parent class, or super class, which holds the
+# behavior in it and inherits this behavior to its children, thus keeping the
+# logic and implementation in a single place.
+
+# For example: Let's say a videogame has multiple character classes and all of
+# then hold a common method, which is sign_in. We can keep yourselves from
+# repeating by creating a class parent to the characters, which we'll call User,
+# that defines this method.
+
+
+class User:
+    def sign_in(self):
+        print("loggin logic")
+
+
+class Wizard(User):
+    def __init__(self, name, power=1):
+        self.name = name
+        self.power = power
+
+    def attack(self):
+        print(f"{self.name}: attacking with power of {self.power}")
+
+
+class Archer(User):
+    def __init__(self, name, arrows_number=0):
+        self.name = name
+        self.arrows_number = arrows_number
+
+    def attack(self):
+        self.arrows_number -= 1
+        print(f"{self.name}: {self.arrows_number} arrows left")
+
+
+char1 = Archer("Legolas", 100)
+char2 = Wizard("Gandalf", 99)
+
+char1.attack()
+char2.attack()
+char1.attack()
+char1.attack()
